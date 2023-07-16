2b52aaa8a7eebdd9f0d5821f8a45a158646c1fa9
Simon Trux <40746371+SimonTrux@users.noreply.github.com>
2023-07-16 22:30:32 +0200

    Update Additional_pkgs.sh

--- Additional_pkgs.sh
+++ Additional_pkgs.sh
@@ -2,6 +2,6 @@
 #!/bin/bash
 
 ## Brave stable repo and browser install
-sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-sudo dnf install brave-browser brave-keyring
+dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+dnf install brave-browser brave-keyring
