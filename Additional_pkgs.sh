d8225195343759ae0045ed83290824a748cbb5ac
Simon Trux <40746371+SimonTrux@users.noreply.github.com>
2023-07-16 22:28:09 +0200

    Update and rename Additional_pkgs.md to Additional_pkgs.sh

--- Additional_pkgs.sh
+++ Additional_pkgs.sh
@@ -1,9 +1,7 @@
-```
+
 #!/bin/bash
 
 ## Brave stable repo and browser install
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo dnf install brave-browser brave-keyring
-
-```
