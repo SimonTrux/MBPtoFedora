7427a96d7c0ccf30e9708ef36f591a73279e57b1
Simon Trux <40746371+SimonTrux@users.noreply.github.com>
2023-07-16 23:18:41 +0200

    Update Additional_pkgs.sh
    added vim chrome joplin and gnome tweaks and extentions

--- Additional_pkgs.sh
+++ Additional_pkgs.sh
@@ -1,7 +1,21 @@
 
 #!/bin/bash
 
+## Vim with profile
+dnf install vim
+curl https://raw.githubusercontent.com/SimonTrux/etc/main/myVimrc > ~/.vimrc
+
 ## Brave stable repo and browser install
 dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-dnf install brave-browser brave-keyring
+dnf install -y brave-browser brave-keyring
+
+## Chrome repo and install 
+dnf config-manager --set-enabled google-chrome
+dnf install -y google-chrome-stable
+
+## Install Joplin note app
+wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash
+
+## Gnome tweaks and extentions apps
+dnf install libappindicator-gtk3 gnome-tweaks gnome-shell-extension-appindicator
